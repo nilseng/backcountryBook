22208d5ee6e5f6d23d02ac1b8e151cfe1af2a697
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2021-01-10 16:42:33 +0100

    testing mapbox map with gpx route in 3D

--- client/src/components/Mapbox.tsx
+++ client/src/components/Mapbox.tsx
@@ -12,17 +12,43 @@ const Mapbox = () => {
     lng: 8.739,
     lat: 61.448,
     zoom: 12,
+    pitch: 85,
+    bearing: 80,
   });
 
   const mapEl = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (mapEl.current && mapboxgl.accessToken) {
-      new mapboxgl.Map({
+      const map = new mapboxgl.Map({
         container: mapEl.current,
         style: "mapbox://styles/nilseng/ckiuk1uf02ykx19szgx4psp19",
         center: [mapState.lng, mapState.lat],
         zoom: mapState.zoom,
+        pitch: 85,
+        bearing: 80,
+      });
+
+      map.on("load", function () {
+        map.addSource("mapbox-dem", {
+          type: "raster-dem",
+          url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+          tileSize: 512,
+          maxzoom: 14,
+        });
+        // add the DEM source as a terrain layer with exaggerated height
+        map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+
+        // add a sky layer that will show when the map is highly pitched
+        map.addLayer({
+          id: "sky",
+          type: "sky",
+          paint: {
+            "sky-type": "atmosphere",
+            "sky-atmosphere-sun": [0.0, 0.0],
+            "sky-atmosphere-sun-intensity": 15,
+          },
+        });
       });
     }
   }, [mapEl, mapState]);
