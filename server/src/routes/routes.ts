f0613de05a712c71a4cfb4e451d30b0b18b0bb55
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2021-01-24 19:18:56 +0100

    converting routes.js to routes.ts

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,11 +1,10 @@
-//Implementing in js instead of ts since @mapbox/togeojson does not seem to have a declaration file
-const express = require("express");
+import express from "express";
 const tj = require("@mapbox/togeojson");
-const fs = require("fs");
+import fs from "fs";
 const DOMParser = require("xmldom").DOMParser;
-const checkJwt = require("../auth/auth").checkJwt;
-const aws = require("aws-sdk");
-const multer = require("multer");
+import { checkJwt } from "../auth/auth"
+import aws from "aws-sdk";
+import multer from "multer";
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -17,19 +16,19 @@ aws.config.update({ region: "eu-west-1" });
 const s3 = new aws.S3();
 const s3Bucket = "bcbookgpx";
 
-router.get("/route/:routeId", async (req, res) => {
+router.get("/route/:routeId", async (req: any, res: any) => {
   if (!req.params.routeId) return res.status(400).json("Missing route id");
   const params = {
     Bucket: s3Bucket,
     Key: req.params.routeId,
   };
-  s3.getObject(params, (err, data) => {
+  s3.getObject(params, (err: any, data: any) => {
     if (err) res.status(404).json({ message: "Could not find geojson" });
     else res.status(200).json(JSON.parse(data.Body.toString()));
   });
 });
 
-router.post("/route", checkJwt, (req, res) => {
+router.post("/route", checkJwt, (req: any, res: any) => {
   if (!req.body.geojsonId || !req.body.geojson) {
     return res.status(400).json({ message: "geojsonId or geojson missing" });
   }
@@ -38,7 +37,7 @@ router.post("/route", checkJwt, (req, res) => {
     Key: req.body.geojsonId,
     Body: Buffer.from(JSON.stringify(req.body.geojson)),
   };
-  s3.upload(uploadParams, (err, data) => {
+  s3.upload(uploadParams, (err: any, data: any) => {
     if (err) {
       res.status(400).json({ Error: err });
     } else {
@@ -51,8 +50,8 @@ router.post(
   "/route/gpxtogeojson",
   checkJwt,
   upload.array("gpx"),
-  (req, res) => {
-    if (!req.files || !req.files.length === 0) {
+  (req: any, res: any) => {
+    if (!req.files || !(req.files.length === 0)) {
       return res.status(400).json({ message: "No file found" });
     }
     const gpxString = req.files[0].buffer.toString();
