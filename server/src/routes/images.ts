b4e41a9b45ca98224cae18411b9e1481709aedcd
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2022-01-24 22:17:20 +0100

    adding image compression

--- server/src/routes/images.ts
+++ server/src/routes/images.ts
@@ -1,11 +1,12 @@
-import express from "express"
-import aws from "aws-sdk"
-import multer from "multer"
+import express from "express";
+import aws from "aws-sdk";
+import multer from "multer";
 import { checkJwt } from "../auth/auth";
+import { compressImage } from "../services/imageService";
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits: { fileSize: 5000000 } });
 
-const router = express.Router()
+const router = express.Router();
 
 // Configuration for aws s3
 aws.config.update({ region: "eu-west-1" });
@@ -13,59 +14,65 @@ const s3 = new aws.S3();
 const defaultBucket = "randohub";
 
 router.get("/image/:key", (req, res) => {
-    if (!req.params.key) return res.status(400).json("Missing object key");
-    const params = {
-        Bucket: req.params.bucket ? req.params.bucket : defaultBucket,
-        Key: req.params.key,
-    };
-    s3.getObject(params, (err: any, data: any) => {
-        if (err) res.status(400).json(err);
-        else res.status(200).send(data.Body);
-    });
+  if (!req.params.key) return res.status(400).json("Missing object key");
+  const params = {
+    Bucket: req.params.bucket ? req.params.bucket : defaultBucket,
+    Key: req.params.key,
+  };
+  s3.getObject(params, (err: any, data: any) => {
+    if (err) res.status(400).json(err);
+    else res.status(200).send(data.Body);
+  });
 });
 
 router.post("/image", checkJwt, upload.array("images"), async (req, res) => {
-    if (!req.body || !req.body.imageIds)
-        return res.status(200).json({ Error: "No images in request" });
-    if (!req.files || req.files.length === 0)
-        return res.status(400).json({ Error: "No files in request" });
-    const uploadParams: any = {
-        Bucket: req.body.bucket ? req.body.bucket : defaultBucket,
-    };
-    if (Array.isArray(req.files)) {
-        for (const [i, file] of req.files.entries()) {
-            uploadParams.Body = file.buffer;
-            if (Array.isArray(req.body.imageIds)) {
-                uploadParams.Key = req.body.imageIds[i];
-            } else {
-                uploadParams.Key = req.body.imageIds;
-            }
-            s3.upload(uploadParams, (err: any, data: any) => {
-                if (err) {
-                    return res.status(400).json({ Error: err })
-                }
-                if (i === (req.files.length as number) - 1)
-                    res.status(200).json("File(s) sucessfully uploaded");
-            });
+  if (!req.body || !req.body.imageIds)
+    return res.status(200).json({ Error: "No images in request" });
+  if (!req.files || req.files.length === 0)
+    return res.status(400).json({ Error: "No files in request" });
+  const uploadParams: any = {
+    Bucket: req.body.bucket ? req.body.bucket : defaultBucket,
+  };
+  if (Array.isArray(req.files)) {
+    for (const [i, file] of req.files.entries()) {
+      try {
+        uploadParams.Body = await compressImage(file.buffer);
+      } catch (e) {
+        return res
+          .status(500)
+          .json({ error: "Something went wrong compressing images." });
+      }
+      if (Array.isArray(req.body.imageIds)) {
+        uploadParams.Key = req.body.imageIds[i];
+      } else {
+        uploadParams.Key = req.body.imageIds;
+      }
+      s3.upload(uploadParams, (err: any, data: any) => {
+        if (err) {
+          return res.status(400).json({ Error: err });
         }
+        if (i === (req.files.length as number) - 1)
+          res.status(200).json("File(s) sucessfully uploaded");
+      });
     }
+  }
 });
 
 router.delete("/images", checkJwt, async (req, res) => {
-    const body = req.body
-    if (!body || !body.imageIds || body.imageIds.length === 0) return res.status(400).json({ error: "No image ids in request" })
-    const params: any = {
-        Bucket: req.body.bucket ? req.body.bucket : defaultBucket,
-        Delete: {
-            Objects: body.imageIds.map((id: string) => ({ Key: id }))
-            ,
-            Quiet: false
-        }
-    };
-    s3.deleteObjects(params, (err: any, data: any) => {
-        if (err) return res.status(400).json({ error: err })
-        else res.status(200).json(`${body.imageIds.length} images deleted`)
-    })
-})
+  const body = req.body;
+  if (!body || !body.imageIds || body.imageIds.length === 0)
+    return res.status(400).json({ error: "No image ids in request" });
+  const params: any = {
+    Bucket: req.body.bucket ? req.body.bucket : defaultBucket,
+    Delete: {
+      Objects: body.imageIds.map((id: string) => ({ Key: id })),
+      Quiet: false,
+    },
+  };
+  s3.deleteObjects(params, (err: any, data: any) => {
+    if (err) return res.status(400).json({ error: err });
+    else res.status(200).json(`${body.imageIds.length} images deleted`);
+  });
+});
 
-export default router
+export default router;
--- server/src/services/imageService.ts
+++ server/src/services/imageService.ts
@@ -1,17 +1,66 @@
-import aws from "aws-sdk"
+import aws from "aws-sdk";
+import { Body } from "aws-sdk/clients/s3";
+import sharp from "sharp";
 
 // Configuration for aws s3
 aws.config.update({ region: "eu-west-1" });
 const s3 = new aws.S3();
 const defaultBucket = "randohub";
 
+const params = {
+  Bucket: defaultBucket,
+};
+
+const imageQuality = 100;
+const maxImageWidth = 1200;
+
 export const deleteImage = (_id: string) => {
-    const params = {
-        Bucket: defaultBucket,
-        Key: _id,
-    };
-    s3.deleteObject(params, (err, data) => {
-        if (err) return { error: err }
-        else return data
-    })
-}
+  const params = {
+    Bucket: defaultBucket,
+    Key: _id,
+  };
+  s3.deleteObject(params, (err, data) => {
+    if (err) return { error: err };
+    else return data;
+  });
+};
+
+export const compressImage = async (buffer: Buffer): Promise<Buffer> => {
+  const compressedImg = await sharp(buffer)
+    .webp({ quality: imageQuality })
+    .resize(maxImageWidth)
+    .toBuffer();
+  return compressedImg;
+};
+
+export const updateImage = async (key: string, body: Body) => {
+  const compressedImage = await compressImage(body as Buffer);
+  const data = await s3
+    .upload({ ...params, Key: key, Body: compressedImage })
+    .promise()
+    .catch((e) => console.log("image update failed.", e));
+  if (data) console.log(`Image with key=${key} updated.`);
+};
+
+export const compressAllImages = (): void => {
+  s3.listObjectsV2(params, (err, data) => {
+    if (!data.Contents) return console.log("No Contents in bucket");
+    console.log(`Compressing ${data.Contents?.length} images...`);
+    for (const imgRef of data.Contents) {
+      if (!imgRef.Key) {
+        console.log(imgRef, " missing Key");
+        continue;
+      }
+      s3.getObject({ ...params, Key: imgRef.Key }, (err, data) => {
+        if (err || !imgRef.Key || !data.Body) {
+          console.log(
+            "Something went wrong when retrieving object with key ",
+            imgRef.Key
+          );
+        } else {
+          updateImage(imgRef.Key, data.Body);
+        }
+      });
+    }
+  });
+};
