642233c1660f0ffd0ea043b50bbc89db63b3616a
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2022-01-24 16:52:00 +0100

    making tests run and adding react testing lib ot dependencies to make heroku behave

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -2,4 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// Mocking the following function since jsdom, used in mapbox-gl, does not support it yet
+window.URL.createObjectURL = jest.fn();
